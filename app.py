7b5eb3ac72d81daa4cc90058e4def06fdef51810
riongabut-lgtm <riongabut@students.amikom.ac.id>
2026-07-16 19:34:55 +0700

    Fix Flask app

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask inport Flask, render_template
+from flask import Flask, render_template
 
-app = Flask (__name__)
+app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template ('index.html')
+    return render_template("index.html")
 
-iff __name__=='__main__'
+if __name__ == "__main__":
     app.run()
